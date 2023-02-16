395ef63003a62466f1c2cf9510aaed15f666ed5e
Nakatsubo <nakatsubo.works@gmail.com>
2023-02-17 00:18:27 +0900

    fix(jsCounterApp): done

---
+++ jsCounterApp/index.js
@@ -0,0 +1,15 @@
+let count = 0;
+
+const countHeader = document.getElementById('count');
+const countAddButton= document.getElementById('add');
+const countSubtractButton= document.getElementById('subtract');
+
+countAddButton.addEventListener('click', () => {
+  count++;
+  countHeader.innerText = count;
+});
+
+countSubtractButton.addEventListener('click', () => {
+  count--;
+  countHeader.innerText = count;
+})
